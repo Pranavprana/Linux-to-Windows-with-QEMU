b64e80c4b38a2da6fcfe8d02fb0f27ef35ad85cf
Pranavprana <91969851+Pranavprana@users.noreply.github.com>
2023-01-27 19:34:48 +0530

    Update and rename 10core.sh to 10coree.sh
    y

--- 10coree.sh
+++ 10coree.sh
@@ -15,27 +15,20 @@ else
 # installing required Ubuntu packages
 dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
 printf "Y\n" | apt-get install sudo -y
-sudo apt-get install vim curl genisoimage -y
+
 # Downloading Portable QEMU-KVM
 echo "Downloading QEMU"
 sudo apt-get update
 sudo apt-get install -y qemu-kvm
-sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
+
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
 sudo wget -O /mediabots/WS2019.ISO https://software-static.download.prss.microsoft.com/sg/download/888969d5-f34g-4e03-ac9d-1f9786c66749/SERVER_EVAL_x64FRE_en-us.iso
-sudo wget -P /floppy https://ftp.mozilla.org/pub/firefox/releases/64.0/win32/en-US/Firefox%20Setup%2064.0.exe
-sudo mv /floppy/'Firefox Setup 64.0.exe' /floppy/Firefox.exe
-sudo wget -P /floppy https://downloadmirror.intel.com/23073/eng/PROWinx64.exe # Intel Network Adapter for Windows Server 2012 R2 
-# Powershell script to auto enable remote desktop for administrator
-sudo touch /floppy/EnableRDP.ps1
-sudo echo -e "Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name \"fDenyTSConnections\" -Value 0" >> /floppy/EnableRDP.ps1
-sudo echo -e "Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\' -Name \"UserAuthentication\" -Value 1" >> /floppy/EnableRDP.ps1
-sudo echo -e "Enable-NetFirewallRule -DisplayGroup \"Remote Desktop\"" >> /floppy/EnableRDP.ps1
+
 # Downloading Virtio Drivers
 sudo wget -P /virtio https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
 # creating .iso for Windows tools & drivers
-sudo mkisofs -o /sw.iso /floppy
+
 #
 #Enabling KSM
 sudo echo 1 > /sys/kernel/mm/ksm/run
@@ -43,4 +36,4 @@ sudo echo 1 > /sys/kernel/mm/ksm/run
 sync; sudo echo 3 > /proc/sys/vm/drop_caches
 # Gathering System information
 sudo /usr/bin/qemu-img create /disk.img 90G
-sudo /usr/bin/qemu-system-x86_64 -net nic -net user,hostfwd=tcp::3389-:3389,hostfwd=tcp::65534-:65534 -m 8721 -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=10 -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -boot d -cdrom /mediabots/WS2019.ISO -hda /disk.img file=/sw.iso,index=3,media=cdrom -vnc :9 & disown %1
+sudo /usr/bin/qemu-system-x86_64 -net nic -net user,hostfwd=tcp::3389-:3389,hostfwd=tcp::65534-:65534 -m 8721 -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=10 -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -boot d -cdrom /mediabots/WS2019.ISO -hda /disk.img -vnc :9 & disown %1
